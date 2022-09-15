b1aeca958e8dbdd85e1998ad4fecb36d7c81af7c
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-09-15 21:35:46 +0600

    timespent fixed

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.round((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
